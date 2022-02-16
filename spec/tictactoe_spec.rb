65d410f11b44b8d4859e173338155f0adc71d1ba
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2022-02-16 13:25:11 +0000

    Remove attribute @board

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -13,7 +13,7 @@ describe TicTacToe do
     position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
     expected_board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
 
-    puts result = tictactoe.display_board()
+    result = tictactoe.display_board()
 
     # Assert
     expect(result).to eq(expected_board)
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,13 +1,12 @@
 class TicTacToe
-    attr_accessor :board, :position
+    attr_accessor :position
 
     def initialize()
         @position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-        @board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
     end    
 
     def display_board()
-        @board
+        board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
     end
 
     def update_gaming_board()
