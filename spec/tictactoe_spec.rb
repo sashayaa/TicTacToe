130377eb8ca2c4f0a86914879bfa1994d8446ad5
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2022-02-20 16:29:47 +0000

    Code Update
    Added is_empty function and modified update_board function.

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,49 +2,113 @@ require 'rspec'
 require './tictactoe'
 
 describe TicTacToe do
-    tictactoe = described_class.new
-
+    
     it "outputs an empty board" do
 
-    # Act
-    position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    expected_board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
+        # Act
+        tictactoe = described_class.new
 
-    result = tictactoe.display_board()
+        position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        result = tictactoe.display_board()
 
-    # Assert
-    expect(result).to eq(expected_board)
+        # Assert
+        expect(result).to eq(expected_board)
 
     end
 
-    it "outputs first turn" do
+    it "outputs board with an 'X' in the first turn at position [0][0]" do
 
         # Act
+        tictactoe = described_class.new
+
         position = [["X", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-        expected_board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
-
-        tictactoe.update_positions(0, 0, "X")
-
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        
+        tictactoe.update_board(0,0,'X')
         result = tictactoe.display_board()
-    
+
         # Assert
         expect(result).to eq(expected_board)
     
     end
 
-    it "outputs first turn" do
+    it "outputs board with an 'X' in the [0][1] position of board" do
 
         # Act
-        position = [["X", "O", " "], [" ", " ", " "], [" ", " ", " "]]
-        expected_board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
+        tictactoe = described_class.new
 
-        tictactoe.update_positions(0, 0, "X")
-        tictactoe.update_positions(0, 1, "O")
-
+        position = [[" ", "X", " "], [" ", " ", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        
+        tictactoe.update_board(0,1,'X')
         result = tictactoe.display_board()
-    
+
         # Assert
         expect(result).to eq(expected_board)
+    
+    end
+
+    it "outputs board with an 'X' and 'O' in separate positions" do
+
+        # Act
+        tictactoe = described_class.new
+        
+        position = [[" ", "X", " "], [" ", "O", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        
+        tictactoe.update_board(0,1,'X')
+        tictactoe.update_board(1,1,'O')
+            
+
+        result = tictactoe.display_board()
+
+        # Assert
+        expect(result).to eq(expected_board)
+    
+    end
+
+    it "checks if position is empty" do
+
+        # Act
+        tictactoe = described_class.new
+        
+        position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+
+        result = tictactoe.is_empty?(1,1)
+
+        # Assert
+        expect(result).to eq(true)
+    
+    end
+
+    it "checks if position is full" do
+
+        # Act
+        tictactoe = described_class.new
+        
+        tictactoe.update_board(1,1,'X')
+
+        result = tictactoe.is_empty?(1,1)
+
+        # Assert
+        expect(result).to eq(false)
+    
+    end
+
+    it "user is prompted to 'try again' when position is full" do
+
+        # Act
+        tictactoe = described_class.new
+        
+        tictactoe.update_board(0,0,'X')
+
+
+        result = tictactoe.update_board(0,0,'O')
+
+        # Assert
+        expect(result).to eq('Position is occupied. Try again!')
     
     end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -5,13 +5,20 @@ class TicTacToe
         @position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
     end    
 
-    def update_positions(row, column, symbol)
-        @position[row][column] = symbol
-        @position
-    end
-
     def display_board()
-        board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
+        board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
     end
+    
+    def update_board(row, column, symbol)
+        if is_empty?(row,column)
+            @position[row][column] = symbol 
+            return @position
+        else 
+            return "Position is occupied. Try again!"
+        end
+    end 
 
+    def is_empty?(row, column)
+        @position[row][column] == " "
+    end
 end
